717a7cc15d2462c7eefb5e872eb4f29fb96998d1
MohammadDwikat1 <mohammaddwikat0059@gmail.com>
2026-04-14 15:40:06 +0300

    hotel list page

---
+++ mobileProject/app/_layout.tsx
@@ -0,0 +1,31 @@
+import { useColorScheme } from '@/hooks/use-color-scheme';
+import { queryClient } from "@/lib/queryClient";
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import { PaperProvider, Portal } from 'react-native-paper';
+import 'react-native-reanimated';
+export const unstable_settings = {
+  anchor: '(tabs)',
+};
+
+export default function RootLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+ <PaperProvider>
+      <Portal.Host>
+      <QueryClientProvider client={queryClient}>
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+      </Stack>
+      <StatusBar style="auto" />
+    </ThemeProvider>
+         </QueryClientProvider>
+         </Portal.Host>
+         </PaperProvider>
+  );
+}
